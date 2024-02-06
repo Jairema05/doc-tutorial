6d149dd5547911427d1865f16aa9e17492cedf17
Jairema05 <jair05wil@hotmail.com>
2024-02-06 12:32:02 -0500

    sesion3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://Jairema05.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doc-tutorial/',
+  baseUrl: '/sesion03/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
